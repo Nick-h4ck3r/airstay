9ee9d6f63f4f736fe75e1a9bc4e14037d03ab12e
Nikhil Kadam <nickkadam23@gmail.com>
2023-08-02 19:08:18 +0530

    vercel.json config

--- api/index.js
+++ api/index.js
@@ -2,14 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 require("dotenv").config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = process.env.JWT_SECRET;
+
+const corsConfig = {
+  origin: true,
+  credentials: true,
+};
+
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors(corsConfig));
+app.options('*', cors(corsConfig))
 
 mongoose.connect(process.env.MONGO_URL);
 
@@ -32,6 +42,36 @@ app.post("/register", async (req, res) => {
     res.json(userDoc);
   } catch (error) {
     res.status(422).json({ ok: false, msg: error.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const userDoc = await User.findOne({ email });
+
+  if (userDoc) {
+    // res.json("found")
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        { expiresIn: "1h" },
+        (err, token) => {
+          if(err) throw err;
+          // res.cookie('token', token).json("password is correct.")
+          res
+          .cookie("token", token, { httpOnly: true })
+          .json("password is correct");
+          console.log(token)
+        }
+      );
+    } else {
+      res.status(422).json("incorrect password");
+    }
+  } else {
+    res.json("not found");
   }
 });
 
