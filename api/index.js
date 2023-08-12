e53f09d99e20c04b74b13b3bd5cd820cf61aad07
Nikhil Kadam <nickkadam23@gmail.com>
2023-08-12 08:33:29 +0530

    removed upload by link /uploads dir

--- api/index.js
+++ api/index.js
@@ -47,7 +47,7 @@ const corsConfig = {
 app.use(express.json());
 app.use(cookieParser());
 // app.use(cors());
-app.use("/uploads", express.static("uploads"));
+// app.use("/uploads", express.static("uploads"));
 app.use(cors(corsConfig));
 app.options("*", cors(corsConfig));
 
@@ -121,22 +121,22 @@ app.post("/logout", async (req, res) => {
   res.cookie("token", "").json(true);
 });
 
-app.post("/upload-by-link", async (req, res) => {
-  const { link } = req.body;
-  const newName = "photo" + Date.now() + ".jpg";
-  await imageDownloader.image({
-    url: link,
-    dest: __dirname + "/uploads/" + newName,
-  });
-  res.json(newName);
-});
+// app.post("/upload-by-link", async (req, res) => {
+//   const { link } = req.body;
+//   const newName = "photo" + Date.now() + ".jpg";
+//   await imageDownloader.image({
+//     url: link,
+//     dest: __dirname + "/uploads/" + newName,
+//   });
+//   res.json(newName);
+// });
 
-const photosMiddleware = multer({ dest: "uploads/" });
+// const photosMiddleware = multer({ dest: "uploads/" });
 
 // app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
 //   const uploadedFiles = [];
 //   for (let i = 0; i < req.files.length; i++) {
-//     const { path, originalname } = req.files[i];
+//     const { path, originalname } = req.files[i];s
 //     const parts = originalname.split(".");
 //     const ext = parts[parts.length - 1];
 //     const newPath = path + "." + ext;
