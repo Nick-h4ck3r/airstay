2c7e44c81ced12b6bbfd24af6d3fa0962a9e7eb1
Nikhil Kadam <nickkadam23@gmail.com>
2024-01-30 01:40:05 +0530

    lucid react icons, add place form changes

--- client/src/pages/Perks.tsx
+++ client/src/pages/Perks.tsx
@@ -1,168 +1,102 @@
+import {
+  CctvIcon,
+  DoorOpenIcon,
+  ParkingCircleIcon,
+  PawPrintIcon,
+  TvIcon,
+  WifiIcon,
+} from "lucide-react";
+
 type Props = {
   selectedPerks: any;
   onChange: (e: any) => void;
 };
 
 function Perks({ selectedPerks, onChange }: Props) {
-  function handleCbClick(e: any) {
-    const { checked, name } = e.target;
-
-    if (checked) {
-      onChange([...selectedPerks, name]);
-    } else {
+  function handleItemClick(name: string) {
+    console.log(selectedPerks);
+    if (selectedPerks.includes(name)) {
       onChange([
         ...selectedPerks.filter((selectedName: any) => selectedName !== name),
       ]);
+    } else {
+      onChange([...selectedPerks, name]);
     }
   }
 
-  const svgClasses = "w-5 md:w-6 h-5 md:h-6";
-
   return (
     <>
-      <label className="border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer">
-        <input
-          type="checkbox"
-          onChange={handleCbClick}
-          checked={selectedPerks.includes("wifi")}
-          name="wifi"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className={svgClasses}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
-          />
-        </svg>
+      <div
+        onClick={() => handleItemClick("wifi")}
+        className={`border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer ${
+          selectedPerks.includes("wifi")
+            ? "bg-[#D70566] text-white"
+            : "bg-gray-100 text-gray-500"
+        } `}
+      >
+        <WifiIcon />
         <span>WiFi</span>
-      </label>
+      </div>
 
-      <label className="border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer">
-        <input
-          type="checkbox"
-          onChange={handleCbClick}
-          checked={selectedPerks.includes("parking")}
-          name="parking"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className={svgClasses}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-          />
-        </svg>
+      <div
+        onClick={() => handleItemClick("parking")}
+        className={`border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer ${
+          selectedPerks.includes("parking")
+            ? "bg-[#D70566] text-white"
+            : "bg-gray-100 text-gray-500"
+        } `}
+      >
+        <ParkingCircleIcon />
         <span>Free parking</span>
-      </label>
+      </div>
 
-      <label className="border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer">
-        <input
-          type="checkbox"
-          onChange={handleCbClick}
-          checked={selectedPerks.includes("tv")}
-          name="tv"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className={svgClasses}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
-          />
-        </svg>
+      <div
+        onClick={() => handleItemClick("tv")}
+        className={`border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer ${
+          selectedPerks.includes("tv")
+            ? "bg-[#D70566] text-white"
+            : "bg-gray-100 text-gray-500"
+        } `}
+      >
+        <TvIcon />
         <span>TV</span>
-      </label>
+      </div>
 
-      <label className="border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer">
-        <input
-          type="checkbox"
-          onChange={handleCbClick}
-          checked={selectedPerks.includes("pets")}
-          name="pets"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className={svgClasses}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-          />
-        </svg>
+      <div
+        onClick={() => handleItemClick("pets")}
+        className={`border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer ${
+          selectedPerks.includes("pets")
+            ? "bg-[#D70566] text-white"
+            : "bg-gray-100 text-gray-500"
+        } `}
+      >
+        <PawPrintIcon />
         <span>Pets allowed</span>
-      </label>
+      </div>
 
-      <label className="border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer">
-        <input
-          type="checkbox"
-          onChange={handleCbClick}
-          checked={selectedPerks.includes("entrance")}
-          name="entrance"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className={svgClasses}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-          />
-        </svg>
+      <div
+        onClick={() => handleItemClick("entrance")}
+        className={`border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer ${
+          selectedPerks.includes("entrance")
+            ? "bg-[#D70566] text-white"
+            : "bg-gray-100 text-gray-500"
+        } `}
+      >
+        <DoorOpenIcon />
         <span>Private entrance</span>
-      </label>
+      </div>
 
-      <label className="border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer">
-        <input
-          type="checkbox"
-          onChange={handleCbClick}
-          checked={selectedPerks.includes("cctv")}
-          name="cctv"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className={svgClasses}
-        >
-          <path
-            strokeLinecap="round"
-            d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-          />
-        </svg>
-
+      <div
+        onClick={() => handleItemClick("cctv")}
+        className={`border flex items-center space-x-2 md:space-x-3 p-3 md:p-4 rounded-xl cursor-pointer ${
+          selectedPerks.includes("cctv")
+            ? "bg-[#D70566] text-white"
+            : "bg-gray-100 text-gray-500"
+        } `}
+      >
+        <CctvIcon />
         <span>CCTV surveillance</span>
-      </label>
+      </div>
     </>
   );
 }
--- client/src/pages/PlacesFormPage.tsx
+++ client/src/pages/PlacesFormPage.tsx
@@ -3,12 +3,13 @@ import { useEffect, useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import Perks from "./Perks";
 import AccountNav from "../AccountNav";
+import { FileUpIcon, Loader2Icon, StarIcon, TrashIcon } from "lucide-react";
 
 type Props = {};
 
 function PlacesFormPage({}: Props) {
   const { id } = useParams();
-  console.log({ id });
+  // console.log({ id });
 
   const [title, setTitle] = useState("");
   const [address, setAddress] = useState("");
@@ -22,6 +23,7 @@ function PlacesFormPage({}: Props) {
   const [photoLink, setPhotoLink] = useState("");
   const [price, setPrice] = useState(100);
   const [redirectToPlacesList, setRedirectToPlacesList] = useState(false);
+  const [imgUploading, setImgUploading] = useState(false);
 
   useEffect(() => {
     if (!id) {
@@ -76,9 +78,13 @@ function PlacesFormPage({}: Props) {
     console.log({ files });
     const data = new FormData();
     data.set("photos", files);
+
+    setImgUploading(true);
+
     for (let i = 0; i < files.length; i++) {
       data.append("photos", files[i]);
     }
+
     axios
       .post("/upload", data, {
         headers: {
@@ -91,6 +97,9 @@ function PlacesFormPage({}: Props) {
           return [...prev, ...filenames];
         });
         console.log(data);
+      })
+      .finally(() => {
+        setImgUploading(false);
       });
   }
 
@@ -127,23 +136,27 @@ function PlacesFormPage({}: Props) {
     return <Navigate to={"/account/places"} />;
   }
 
-  function removePhoto(link: any) {
+  function removePhoto(e: any, link: any) {
+    e.preventDefault();
     setAddedPhotos((prev: any) => {
       return prev.filter((item: any) => item !== link);
     });
   }
 
-  function selectAsMainPhoto(e: any, link: any) {
-    e.preventDefault();
-    setAddedPhotos((prev: any) => {
-      return [link, ...prev.filter((item: any) => item !== link)];
-    });
-  }
+  // function selectAsMainPhoto(e: any, link: any) {
+  //   e.preventDefault();
+  //   setAddedPhotos((prev: any) => {
+  //     return [link, ...prev.filter((item: any) => item !== link)];
+  //   });
+  // }
 
   return (
     <div>
       <AccountNav />
-      <form onSubmit={savePlace} className="max-w-6xl mx-auto px-2 md:px-0">
+      <form
+        onSubmit={savePlace}
+        className="max-w-6xl mx-auto px-2 md:px-0"
+      >
         {preInput("Title", "Title for your places should be catchy.")}
         <input
           className=" "
@@ -163,28 +176,34 @@ function PlacesFormPage({}: Props) {
         />
 
         {preInput("Photos", "show off cool images of your place!")}
-        <div className="flex gap-2">
-          <input
-            value={photoLink}
-            onChange={(e) => setPhotoLink(e.target.value)}
-            type="text"
-            placeholder="add form the link ...jpg"
-          />
-          <button
-            onClick={addPhotoByLink}
-            className={`bg-gray-200 px-4 md:px-8 text-sm md:text-base rounded-2xl my-1 ${
-              !photoLink && "opacity-50"
-            }`}
-            disabled={!photoLink}
-          >
-            add&nbsp;photo
-          </button>
-        </div>
+        <>
+          {/* add photo by link */}
+          <div className="flex gap-2">
+            <input
+              value={photoLink}
+              onChange={(e) => setPhotoLink(e.target.value)}
+              type="text"
+              placeholder="add form the link ...jpg"
+            />
+            <button
+              onClick={addPhotoByLink}
+              className={`bg-gray-200 px-4 md:px-8 text-sm md:text-base rounded-2xl my-1 ${
+                !photoLink && "opacity-50"
+              }`}
+              disabled={!photoLink}
+            >
+              add&nbsp;photo
+            </button>
+          </div>
+        </>
 
         <div className="mt-2 gap-2 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
           {addedPhotos.length > 0 &&
             addedPhotos.map((link: any) => (
-              <div key={link} className="h-32 flex relative">
+              <div
+                key={link}
+                className="h-32 flex relative"
+              >
                 <img
                   className="rounded-2xl w-full object-cover"
                   // src={"http://localhost:4000/uploads/" + link}
@@ -192,83 +211,42 @@ function PlacesFormPage({}: Props) {
                 />
 
                 <button
-                  onClick={() => removePhoto(link)}
+                  onClick={(e) => removePhoto(e, link)}
                   className="absolute bottom-2 right-2 p-1 shadow-md shadow-black bg-slate-800/60 rounded-2xl text-white"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="w-[18px] h-[18px]"
+                  <TrashIcon className="h-4 w-4" />
+                </button>
+
+                {link === addedPhotos[0] && (
+                  <button
+                    // onClick={(e) => selectAsMainPhoto(e, link)}
+                    className="absolute bottom-2 left-2 p-1 shadow-md shadow-black bg-slate-800/60 rounded-2xl text-white"
                   >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    <StarIcon
+                      className={`h-4 w-4 fill-[#D70566] stroke-[#D70566]`}
                     />
-                  </svg>
-                </button>
-                <button
-                  onClick={(e) => selectAsMainPhoto(e, link)}
-                  className="absolute bottom-2 left-2 p-1 shadow-md shadow-black bg-slate-800/60 rounded-2xl text-white"
-                >
-                  {link === addedPhotos[0] && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="w-[18px] h-[18px] text-primary"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  )}
-                  {link !== addedPhotos[0] && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="w-[18px] h-[18px]"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                      />
-                    </svg>
-                  )}
-                </button>
+                  </button>
+                )}
               </div>
             ))}
 
-          <label className="h-32 inline-flex items-center justify-center gap-1 md:gap-3 border rounded-2xl md:p-8 text-sm md:text-2xl font-bold text-gray-600  cursor-pointer">
+          {imgUploading && (
+            <div className="flex flex-col h-32 items-center justify-center border rounded-2xl text-sm text-center text-gray-600 gap-2 px-4">
+              <div className="flex items-center justify-center">
+                <Loader2Icon className="animate-spin" />
+              </div>
+              <p>Uploading image, please wait...</p>
+            </div>
+          )}
+
+          <label className="h-32 inline-flex items-center justify-center gap-1 md:gap-3 border rounded-2xl md:p-8 text-sm md:text-2xl font-bold text-gray-600 cursor-pointer hover:bg-black/5 duration-200">
             <input
               type="file"
               multiple
               className="hidden"
               onChange={uploadPhoto}
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-5 md:w-8 h-5 md:h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-              />
-            </svg>
+            <FileUpIcon />
 
             <p>Upload</p>
           </label>
@@ -291,7 +269,10 @@ function PlacesFormPage({}: Props) {
           "What makes your place special? Select all that apply."
         )}
         <div className="mt-2 mb-6 grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-xs md:text-base">
-          <Perks selectedPerks={perks} onChange={setPerks} />
+          <Perks
+            selectedPerks={perks}
+            onChange={setPerks}
+          />
         </div>
 
         {preInput(
