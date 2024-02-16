75d0a3fec281caa6e46cdec34a7694e75944d46e
Nikhil Kadam <nickkadam23@gmail.com>
2024-02-16 17:28:19 +0530

    feat: lucid icons, readme

--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -3,14 +3,21 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../LoadingSpinner";
+import {
+  CctvIcon,
+  DoorOpenIcon,
+  HeartIcon,
+  ParkingCircleIcon,
+  PawPrintIcon,
+  TvIcon,
+  WifiIcon,
+} from "lucide-react";
 
 type Props = {};
 
 function IndexPage({}: Props) {
   const [places, setPlaces] = useState<any>([]);
   const [loading, setLoading] = useState(true);
-
-  const svgClasses = "w-[14px] h-[14px]";
 
   useEffect(() => {
     console.log("IndexPage");
@@ -38,7 +45,7 @@ function IndexPage({}: Props) {
       {loading ? (
         <LoadingSpinner msg="Hold still, we are finding the perfect places for you...." />
       ) : (
-        <div className="gap-x-6 gap-y-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        <div className="gap-x-6 gap-y-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
           {places.length > 0 &&
             places.map((place: any, index: number) => (
               <Link
@@ -65,22 +72,11 @@ function IndexPage({}: Props) {
 
                     <button
                       onClick={() => addToFav()}
-                      className="absolute right-3 top-3 text-gray-100 z-30"
+                      className="absolute right-4 top-4 text-gray-100 z-30"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6 border p-[3px] border-white/70 rounded-full bg-gray-500/30 shadow-lg shadow-gray-500/30"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                        />
-                      </svg>
+                      <div>
+                        <HeartIcon />
+                      </div>
                     </button>
                   </div>
 
@@ -110,103 +106,24 @@ function IndexPage({}: Props) {
                     {place.perks.length > 0 && (
                       <div className="grid grid-cols-3 gap-3">
                         {place.perks.map((perk: any) => (
-                          <div className="inline-flex gap-2 items-center bg-gray-200/50 text-center rounded-full px-0 justify-center py-1 text-xs text-gray-600 border border-gray-200/70">
+                          <div className="inline-flex gap-1.5 items-center bg-gray-200/50 text-center rounded-full px-0 justify-center py-1 text-xs text-gray-600 border border-gray-200/70">
                             {perk === "wifi" && (
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className={svgClasses}
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
-                                />
-                              </svg>
+                              <WifiIcon className="h-4 w-4" />
                             )}
                             {perk === "parking" && (
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className={svgClasses}
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-                                />
-                              </svg>
+                              <ParkingCircleIcon className="h-4 w-4" />
                             )}
-                            {perk === "tv" && (
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className={svgClasses}
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
-                                />
-                              </svg>
-                            )}
+                            {perk === "tv" && <TvIcon className="h-4 w-4" />}
                             {perk === "pets" && (
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className={svgClasses}
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-                                />
-                              </svg>
+                              <PawPrintIcon className="h-4 w-4" />
                             )}
                             {perk === "entrance" && (
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className={svgClasses}
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-                                />
-                              </svg>
+                              <DoorOpenIcon className="h-4 w-4" />
                             )}
                             {perk === "cctv" && (
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className={svgClasses}
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-                                />
-                              </svg>
+                              <CctvIcon className="h-4 w-4" />
                             )}
-                            <span>{perk}</span>
+                            <span className="hidden md:block">{perk}</span>
                           </div>
                         ))}
                       </div>
