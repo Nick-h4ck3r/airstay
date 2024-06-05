a8a6401a5e958e3d84805cee714f7bf81fb3b2f7
Nikhil Kadam <nickkadam23@gmail.com>
2024-06-06 05:04:12 +0530

    fix: reformat export of pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,20 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import IndexPage from "./pages/IndexPage";
-import LoginPage from "./pages/LoginPage";
-import Layout from "./Layout";
-import RegisterPage from "./pages/RegisterPage";
 import axios from "axios";
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./Layout";
 import { UserContextProvider } from "./UserContext";
-import ProfilePage from "./pages/ProfilePage";
-import PlacesPage from "./pages/PlacesPage";
-import PlacesFormPage from "./pages/PlacesFormPage";
-import PlacePage from "./pages/PlacePage";
-import BookingsPage from "./pages/BookingsPage";
-import BookingPage from "./pages/BookingPage";
+
+import {
+  IndexPage,
+  BookingPage,
+  BookingsPage,
+  LoginPage,
+  PlacePage,
+  PlacesFormPage,
+  PlacesPage,
+  ProfilePage,
+  RegisterPage,
+} from "./pages/index";
 
 // const clientHostname = window.location.hostname;
 // const isProduction = clientHostname === 'airstay.vercel.app';
@@ -22,28 +25,64 @@ import BookingPage from "./pages/BookingPage";
 
 // axios.defaults.baseURL = "http://localhost:4000";
 // axios.defaults.baseURL = "https://airstay.cyclic.app";
+
 axios.defaults.baseURL = "https://airstay-api.vercel.app";
 axios.defaults.withCredentials = true;
-
 axios.defaults.timeout = 50000;
 
 function App() {
   return (
     <UserContextProvider>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<IndexPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/account" element={<ProfilePage />} />
-          <Route path="/account/places" element={<PlacesPage />} />
-          <Route path="/account/places/new" element={<PlacesFormPage />} />
-          <Route path="/account/places/:id" element={<PlacesFormPage />} />
-          <Route path="/place/:id" element={<PlacePage />} />
-          <Route path="/account/bookings" element={<BookingsPage />} />
-          <Route path="/account/bookings/:id" element={<BookingPage />} />
+        <Route
+          path="/"
+          element={<Layout />}
+        >
+          <Route
+            index
+            element={<IndexPage />}
+          />
+          <Route
+            path="/login"
+            element={<LoginPage />}
+          />
+          <Route
+            path="/register"
+            element={<RegisterPage />}
+          />
+          <Route
+            path="/account"
+            element={<ProfilePage />}
+          />
+          <Route
+            path="/account/places"
+            element={<PlacesPage />}
+          />
+          <Route
+            path="/account/places/new"
+            element={<PlacesFormPage />}
+          />
+          <Route
+            path="/account/places/:id"
+            element={<PlacesFormPage />}
+          />
+          <Route
+            path="/place/:id"
+            element={<PlacePage />}
+          />
+          <Route
+            path="/account/bookings"
+            element={<BookingsPage />}
+          />
+          <Route
+            path="/account/bookings/:id"
+            element={<BookingPage />}
+          />
         </Route>
-        <Route path="*" element={<div>404</div>} />
+        <Route
+          path="*"
+          element={<div>404</div>}
+        />
       </Routes>
     </UserContextProvider>
   );
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,24 @@
+import BookingPage from './BookingPage';
+import BookingsPage from './BookingsPage';
+import IndexPage from './IndexPage';
+import LoginPage from './LoginPage';
+import Perks from './Perks';
+import PlacesPage from './PlacesPage';
+import PlacesFormPage from './PlacesFormPage';
+import PlacePage from './PlacePage';
+import ProfilePage from './ProfilePage';
+import RegisterPage from './RegisterPage';
+
+
+export {
+  BookingPage,
+  BookingsPage,
+  IndexPage,
+  LoginPage,
+  Perks,
+  PlacesPage,
+  PlacesFormPage,
+  PlacePage,
+  ProfilePage,
+  RegisterPage,
+};
